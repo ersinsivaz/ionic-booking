cc5cc6640af2c4c98e756b03214e72178b36e2a4
ersin sivaz <ersinsivaz@hotmail.com>
2019-03-21 16:06:32 +0300

    108. de kaldım

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -10,21 +10,21 @@ export class PlacesService {
       'p1',
       'Manhattan mansion',
       'In the heart of the New York city',
-      'https://loremflickr.com/320/240',
+      'https://loremflickr.com/320/240?random=1',
       149.99
     ),
     new Place(
       'p2',
       'Lamour Toujours',
       'A romantic place in Paris',
-      'https://loremflickr.com/320/240',
+      'https://loremflickr.com/320/240?random=2',
       189.99
     ),
     new Place(
       'p3',
       'The Foggy Palace',
       'Not your average city trip!',
-      'https://loremflickr.com/320/240',
+      'https://loremflickr.com/320/240?random=3',
       99.99
     ),
   ];
