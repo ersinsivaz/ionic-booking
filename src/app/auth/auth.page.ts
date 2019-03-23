93577f7b88cdd22ec1a44b03e8e14d82aef82e30
ersin sivaz <ersinsivaz@hotmail.com>
2019-03-24 00:31:57 +0300

    162. creating reactive form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -11,6 +11,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
   constructor(private authService: AuthService,
     private router: Router,
     private loadingCtrl: LoadingController) { }
@@ -33,7 +34,24 @@ export class AuthPage implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    //console.log(form);
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if (this.isLogin) {
+      //send a request to login   
+    } else {
+      //send a request to signup   
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 
 }
