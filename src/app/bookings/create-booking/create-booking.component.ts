b06c29ed74e9a48e522fb11c9fc5314976883fdb
ersin sivaz <ersinsivaz@hotmail.com>
2019-03-24 15:05:23 +0300

    173 managing state

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Place } from '../../places/place.model';
 import { ModalController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-booking',
@@ -9,17 +10,51 @@ import { ModalController } from '@ionic/angular';
 })
 export class CreateBookingComponent implements OnInit {
   @Input() selectedPlace: Place;
+  @Input() selectedMode: string;
+  @ViewChild('f') form: NgForm;
+  startDate: string;
+  endDate: string;
 
   constructor(private modalCtrl: ModalController) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const availableFrom = new Date(this.selectedPlace.availableFrom);
+    const availableTo = new Date(this.selectedPlace.availableTo);
+    if (this.selectedMode === 'random') {
+      this.startDate = new Date(
+        availableFrom.getTime() +
+          Math.random() * (availableTo.getTime() - 7 * 24 * 60 * 60 * 1000 - availableFrom.getTime())
+        ).toISOString();
+
+      this.endDate = new Date(
+      new Date(this.startDate).getTime() +
+        Math.random() * (new Date(this.startDate).getTime() + 6 * 24 * 60 * 60 * 1000 - new Date(this.startDate).getTime()
+        )).toISOString();
+    }
+
+  }
 
   onCancel() {
     this.modalCtrl.dismiss(null, 'cancel');
   }
 
   onBookPlace() {
-    this.modalCtrl.dismiss({message: 'This is a dummy message!'}, 'confirm');
+    if (!this.form.valid || !this.datesValid()) {
+      return;
+    }
+    this.modalCtrl.dismiss({bookingData: {
+      firstName: this.form.value['firstName'],
+      lastName: this.form.value['lastName'],
+      guestNumber: this.form.value['guestNumber'],
+      startDate: this.form.value['dateFrom'],
+      endDate: this.form.value['dateTo'],
+    }}, 'confirm');
+  }
+
+  datesValid() {
+    const startDate = new Date(this.form.value['dateFrom']);
+    const endDate = new Date(this.form.value['dateTo']);
+    return endDate > startDate;
   }
 
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -11,21 +11,27 @@ export class PlacesService {
       'Manhattan mansion',
       'In the heart of the New York city',
       'https://loremflickr.com/320/240?random=1',
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p2',
       'Lamour Toujours',
       'A romantic place in Paris',
       'https://loremflickr.com/320/240?random=2',
-      189.99
+      189.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p3',
       'The Foggy Palace',
       'Not your average city trip!',
       'https://loremflickr.com/320/240?random=3',
-      99.99
+      99.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
   ];
 
